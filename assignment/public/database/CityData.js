0ae4a68f38120fdcb63180c6e7a4a5866be684af
anjnaharsora <lanetteam.anjnaharsora@gmail.com>
2017-02-03 18:26:14 +0530

    complete assignment(insert,update,display,delete)

---
+++ assignment/public/database/CityData.js
@@ -0,0 +1,17 @@
+// grab the things we need
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// create a schema
+var citySchema = new Schema({
+    id : Number,
+    name  : String,
+    state_id: Number
+
+});
+
+// the schema is useless so far
+// we need to create a model using it
+module.exports = mongoose.model('City', citySchema);
+
+// make this available to our users in our Node applications
